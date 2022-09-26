31508efddeac83678ceb6822a0c2b87531553cd6
YasmineRico <ricolyasmine@gmail.com>
2022-09-26 18:25:52 -0500

    add source package and pom dependencies

---
+++ src/main/java/com/codeup/bookwormapp/BookWormAppApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.bookwormapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWormAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookWormAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/bookwormapp/BookWormAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.bookwormapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWormAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
