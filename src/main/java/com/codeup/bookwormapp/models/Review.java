37b4009cbf6b1faa29b0fdc5963e196fbd608de7
YasmineRico <ricolyasmine@gmail.com>
2022-09-28 02:42:42 -0500

    complete Models, add dependencies on pom.xml, and connect database

---
+++ src/main/java/com/codeup/bookwormapp/models/Review.java
@@ -0,0 +1,111 @@
+package com.codeup.bookwormapp.models;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private long rating;
+
+    @Column(nullable = false, length = 100)
+    private String reviewTitle;
+
+    @Column(length = 2500)
+    private String reviewBody;
+
+
+    @Column(name = "PUBLISHED_DATE")
+    private LocalDateTime publishedDate;
+
+
+    @ManyToOne
+    @JoinColumn (name = "book_id")
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    //    Empty Constructor
+    public Review() {
+    }
+
+//    Constructor
+
+
+    public Review(Long id, long rating, String reviewTitle, String reviewBody, Book book, LocalDateTime publishedDate, User user) {
+        this.id = id;
+        this.rating = rating;
+        this.reviewTitle = reviewTitle;
+        this.reviewBody = reviewBody;
+        this.book = book;
+        this.publishedDate = publishedDate;
+        this.user = user;
+    }
+
+//    Getters and Setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getRating() {
+        return rating;
+    }
+
+    public void setRating(long rating) {
+        this.rating = rating;
+    }
+
+    public String getReviewTitle() {
+        return reviewTitle;
+    }
+
+    public void setReviewTitle(String reviewTitle) {
+        this.reviewTitle = reviewTitle;
+    }
+
+    public String getReviewBody() {
+        return reviewBody;
+    }
+
+    public void setReviewBody(String reviewBody) {
+        this.reviewBody = reviewBody;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public LocalDateTime getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDateTime publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/codeup/bookwormapp/models/User.java
@@ -0,0 +1,203 @@
+package com.codeup.bookwormapp.models;
+
+import javax.persistence.*;
+import java.util.List;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "USER_ID", nullable= false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="EMAIL",nullable = false)
+    private String email;
+
+    @Column(name = "FIRST_NAME", nullable = false)
+    private String firstName;
+
+    @Column (name = "LAST_NAME", nullable=false)
+    private String lastName;
+
+    @Column(name="USERNAME", nullable = false,unique = true, length = 16)
+    private String username;
+
+    @Column(name="PASSWORD", nullable = false, length = 100)
+    private String password;
+
+    @Column(name = "BIRTHDAY", nullable = false)
+    private LocalDate birthday;
+
+    @Column (name="FAVORITE_BOOK")
+    private String favoriteBook;
+
+    @Column (name="CITY", nullable = false)
+    private String city;
+
+    @Column(name = "STATE", nullable = false)
+    private String state;
+
+    @Column(name = "BIO")
+    private String bio;
+
+    @Column(name = "FAVORITE_GENRE")
+    private String favoriteGenre;
+
+//LOOK UP MORE INFORMATION for data type************
+    @Column(name = "PROFILE_PHOTO", nullable = true)
+    private String profilePhoto;
+
+    @Column(name="SOCIAL_MEDIA")
+    private String socialMedia;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Review> reviews;
+
+//    Empty Constructor
+    public User(){
+    }
+
+//    Constructor
+
+    public User(long id, String email, String firstName, String lastName, String username, String password, LocalDate birthday, String favoriteBook, String city, String state, String bio, String favoriteGenre, String profilePhoto, String socialMedia, List<Review> reviews) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.birthday = birthday;
+        this.favoriteBook = favoriteBook;
+        this.city = city;
+        this.state = state;
+        this.bio = bio;
+        this.favoriteGenre = favoriteGenre;
+        this.profilePhoto = profilePhoto;
+        this.socialMedia = socialMedia;
+        this.reviews = reviews;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getFavoriteBook() {
+        return favoriteBook;
+    }
+
+    public void setFavoriteBook(String favoriteBook) {
+        this.favoriteBook = favoriteBook;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getFavoriteGenre() {
+        return favoriteGenre;
+    }
+
+    public void setFavoriteGenre(String favoriteGenre) {
+        this.favoriteGenre = favoriteGenre;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getSocialMedia() {
+        return socialMedia;
+    }
+
+    public void setSocialMedia(String socialMedia) {
+        this.socialMedia = socialMedia;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+}
