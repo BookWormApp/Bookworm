b9cb1e4bc6397f3d234a4c1c7b3f9841b69055a4
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-28 20:56:57 -0500

    adding the index html

---
+++ src/main/java/com/codeup/bookwormapp/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.codeup.bookwormapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+
+    //-- Index Page
+    @GetMapping("/")
+    @ResponseBody
+    public String index(Model model){
+        return " Hello World";
+    }
+
+}
