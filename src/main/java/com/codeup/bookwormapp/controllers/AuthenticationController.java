52cb94e3a03f1117c4b3d639e3deb804135af9ae
YasmineRico <ricolyasmine@gmail.com>
2022-09-29 01:59:34 -0500

    link views to authentication controller and user controller

--- src/main/java/com/codeup/bookwormapp/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/bookwormapp/controllers/AuthenticationController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class AuthenticationController {
 
     @GetMapping("/login")
-    @ResponseBody
     public String loginForm(){
-        return "This is to login";
+        return "users/login";
     }
 
 }
--- src/main/java/com/codeup/bookwormapp/controllers/UserController.java
+++ src/main/java/com/codeup/bookwormapp/controllers/UserController.java
@@ -9,15 +9,13 @@ public class UserController {
 
 
     @GetMapping("/register")
-    @ResponseBody
     public String signupForm(){
-        return "this is the signup form";
+        return "users/register";
     }
 
     @GetMapping("/profile")
-    @ResponseBody
     public String profile(){
-        return "User Profile";
+        return "users/profile";
     }
 
 
