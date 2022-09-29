74f29d6e4176936e936617fc75382d9ab54230c1
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-09-29 04:02:27 -0500

    create a seeder for users

--- seeder.sql
+++ seeder.sql
@@ -16,5 +16,14 @@ INSERT INTO users (bio, birthday, city, email, favorite_book, favorite_genre, fi
 ('I love Books', '1956-11-08', 'Arlington', 'ama@email.com', 'The Queen', 'Biographies', 'Maria', 'Vasquez', 'ama210', 'Virginia', 'ama210'),
 ('Books give me joy', '1988-03-15', 'Boise', 'angel@email.com', 'Harry Potter', 'fantasy', 'Angel', 'Vasquez', 'angelzz', 'Idaho','angelUser'),
 ('Pet Parent', '1922-04-28', 'Clinton','mary@email.com', 'Goosebumps', 'Thriller', 'Craig', 'Baskin', 'craigsKool', 'Maryland', 'craigUser'),
-('I love BookWorm Website!!', '1890-01-01', 'Boston', 'book@email.com', 'Diary of Anne Frank', 'Romance', 'Lilibet', 'Windsor', 'password123', 'Massachusets', 'NotTheQueen')                              
-
+('I love BookWorm Website!!', '1890-01-01', 'Boston', 'book@email.com', 'Diary of Anne Frank', 'Romance', 'Lilibet', 'Windsor', 'password123', 'Massachusets', 'NotTheQueen'),
+('Do I need to read! YES I DO', '2003-01-12', 'Anaheim ', 'user@email.com', 'Coraline', 'Thriller', 'Stan', 'Gilbertson', 'password', 'California', 'angelcitydude'),
+('Because you kill me You know you do, you fuck', '1992-06-29', 'Dallas', 'knownReader@email.com', 'The Road', 'Sci-Fi', 'Bobby', 'Smith', 'password', 'Texas', 'knownReader' ),
+('who needs a bio?!?!', '1989-12-20' , 'dallas', 'dallasboy@email.com', 'lonestar', 'comedy', 'luke', 'lacey', 'password', 'texas', 'dallasboy'),
+('live life like no one is watching', '2001-02-05','los angeles', 'losAngel@email.com', 'The Road', 'comedy', 'Chris', 'Blake', 'password', 'california', 'losAngel'),
+('One day I will make it big just watch', '1990-03-23', 'arlington', 'shooterstar@email.com', 'hocus pocus', 'horror', 'samantha', 'butters', 'password', 'texas', 'samsam'),
+('well I tell you my friend there will be one day', '2004-09-22', 'boston', 'lonewolf@email.com', 'harry potter', 'fiction', 'christy', 'daily', 'password','Massachusetts', 'dailyLiving'),
+('funny quotes make great introductions', '1995-04-14', 'san antonio', 'whoamI@email.com', 'the dark path taken', 'mystery', 'bobby', 'gilmore', 'password', 'texas', 'happygilmore'),
+('make great introductions', '1995-05-11','san antonio', 'wowthisiscool@email.com', 'the path', 'Romance', 'lucy', 'smith', 'password', 'texas', 'lucylu'),
+('life is what you make it homie','1991-07-30', 'roswell', 'wildhog@email.com', 'The Road', 'Biography', 'kyle', 'lara', 'password', 'New Mexico', 'wildhog'),
+('simple reading makes life easy', '1998-04-18','new york', 'newworld@email.com', 'cat in the hat', 'adventure', 'Lucas', 'Patterson', 'password','new york', 'newworld')
