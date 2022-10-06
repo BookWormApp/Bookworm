a56d4cfb61cf38058413d995828112d6a1cd5707
YasmineRico <ricolyasmine@gmail.com>
2022-10-05 23:33:29 -0500

    add categories section to home controller and update thymeleaf in index page

--- src/main/java/com/codeup/bookwormapp/controllers/HomeController.java
+++ src/main/java/com/codeup/bookwormapp/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.codeup.bookwormapp.controllers;
 
+import com.codeup.bookwormapp.models.Book;
 import com.codeup.bookwormapp.repository.BookRepository;
 import com.codeup.bookwormapp.repository.ReviewRepository;
 import org.springframework.data.domain.Sort;
@@ -7,6 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Controller
 public class HomeController {
@@ -28,9 +34,22 @@ public class HomeController {
         model.addAttribute("books", bookDao.findAll(Sort.by("rating").descending()));
         model.addAttribute("reviews", reviewDao.findAll());
         model.addAttribute("book", bookDao.findAll(Sort.by("genre").descending()));
+        model.addAttribute("bookGenre", getGenres(bookDao.findAll()));
         return "home/index";
     }
 
+
+
+    public Set<String>  getGenres(List<Book> books){
+        //-- Created a new list nothing in it.
+        List<String> genres = new ArrayList<>();
+        //-- for loop that adds all books genre to a list
+        for (Book b : books){genres.add(b.getGenre());}
+        //-- Turn the list into a Set
+        Set<String> genreSet = new HashSet<String>(genres);
+        //-- return the set
+        return genreSet;
+    }
 
 
     //-- About Page
